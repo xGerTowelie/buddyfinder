4bd934a758620c2568d83c1cbc812a6dfbdde6f6
towelie <mail@towelie.dev>
2024-09-19 21:23:26 +0200

    added black background for welcome screen makes it more relaxed for the eyes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
             >
                 {children}
             </body>
